4b72f245dc6a0ace243d90e767ebccbc0571b617
morizkraemer <morizkraemer@gmail.com>
2025-02-14 00:17:26 +0100

    update build script

--- buildwindows.bat
+++ buildwindows.bat
@@ -4,6 +4,15 @@ setlocal
 :: Load variables from build-config.env
 for /f "tokens=1,2 delims==" %%a in (build-config.env) do set %%a=%%b
 
+if not exist "%RUNTIME_IMAGE%" (
+    echo Creating custom runtime...
+    jlink --output "%RUNTIME_IMAGE%" --module-path "%JAVA_HOME%\jmods" --add-modules java.base,java.desktop,java.naming
+    if %errorlevel% neq 0 (
+        echo ERROR: jlink failed to create custom runtime!
+        exit /b 1
+    )
+)
+
 :: Run jpackage
 jpackage ^
     --type exe ^
